2c2f2a6606018b8c05bf1af7166c1ee57c32d414
Sabrina Cazzolato <sabca270@student.liu.se>
2022-02-10 13:56:46 +0100

    Created lab03 folder

---
+++ lab03/server.py
@@ -0,0 +1,226 @@
+from flask import Flask, jsonify, request
+import database_helper
+from database_helper import DatabaseErrorCode
+import utils
+
+app = Flask(__name__)
+
+app.debug = True
+
+@app.teardown_request
+def after_request(exception):
+    database_helper.close_session();
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    json = request.get_json()
+
+    if "username" not in json or "password" not in json:
+        return "{}", 400 #Bad Request
+
+    if database_helper.read_logged_in_user(json['username']) is not None:
+        return "{}", 409 #Conflict
+
+    user = database_helper.read_user(json["username"])
+
+    if user is None or user.password != json["password"]:
+        return "{}", 403 #Forbidden
+
+    token = utils.generate_token()
+
+    result = database_helper.create_logged_in_user(json["username"], token)
+
+    if result != DatabaseErrorCode.Success:
+        return "{}", 500 #Internal Server Error
+
+    response_body = "{token: %s}" % token
+
+    return response_body, 200 #OK
+
+
+@app.route('/sign_up', methods=['POST'])
+def sign_up():
+    json = request.get_json()
+
+    for key in ["email", "password", "first_name", "family_name", "gender", "city", "country"]:
+        if key not in json or len(json[key]) > 255:
+            return "{}", 400 #Bad Request
+
+
+    if database_helper.read_user(json["email"]) is not None:
+        return "{}", 409 #Conflict
+
+    user_info = {
+        "email": json["email"],
+        "password": json["password"],
+        "first_name": json["first_name"],
+        "family_name": json["family_name"],
+        "gender": json["gender"],
+        "city": json["city"],
+        "country": json["country"]
+    }
+
+    result = database_helper.create_user(user_info)
+
+    if result != DatabaseErrorCode.Success:
+        return "{}", 500 #Internal Server Error
+
+    return jsonify(user_info), 201 #Created successfully
+
+@app.route('/sign_out', methods=['DELETE'])
+def sign_out():
+    headers = request.headers
+    if "Authorization" not in headers:
+        return "{}", 400 #Bad Request
+
+    user = database_helper.read_user_by_token(headers["Authorization"])
+
+    if user is None:
+        return "{}", 404 #Not Found
+
+    if database_helper.delete_logged_in_user(user.email) != DatabaseErrorCode.Success:
+        return "{}", 500 #Internal Server Error
+
+    return "{}", 200 #OK
+
+@app.route('/change_password', methods=["PUT"])
+def change_password():
+    json = request.get_json()
+    headers = request.headers
+
+    if "Authorization" not in headers or "old_password" not in json or "new_password" not in json:
+        return "{}", 400 #Bad Request
+
+    user = database_helper.read_user_by_token(headers["Authorization"])
+
+    if user is None or user.password != json["old_password"]:
+        return "{}", 403 # Forbidden
+
+    if database_helper.update_user_password(user.email, json["new_password"]) != DatabaseErrorCode.Success:
+        return "{}", 500 #Internal Server Error
+
+    return "{}", 200 # OK
+
+@app.route('/get_user_data_by_token', methods=['GET'])
+def get_user_data_by_token():
+    headers = request.headers
+
+    if "Authorization" not in headers:
+        return "{}", 400 #Bad Request
+
+    token = headers.get("Authorization")
+
+    user = database_helper.read_user_by_token(token)
+
+    if user is None:
+        return "{}", 403 #Forbidden
+
+    user_info = {
+        "email": user.email,
+        "first_name": user.first_name,
+        "family_name": user.family_name,
+        "gender": user.gender,
+        "city": user.city,
+        "country": user.country
+    }
+
+    return jsonify(user_info), 200 #OK
+
+
+@app.route('/get_user_data/<email>', methods=['GET'])
+def get_user_data_by_email(email):
+    headers = request.headers
+
+    if "Authorization" not in headers:
+        return "{}", 400 #Bad Request
+
+    token = headers.get("Authorization")
+
+    if database_helper.read_user_by_token(token) is None:
+        return "{}", 403 #Forbidden
+
+    user = database_helper.read_user(email)
+
+    if user is None:
+        return "{}", 404 #Not Found
+    else:
+        user_info = {
+            "email": user.email,
+            "first_name": user.first_name,
+            "family_name": user.family_name,
+            "gender": user.gender,
+            "city": user.city,
+            "country": user.country
+        }
+
+        return jsonify(user_info), 200 #Success
+
+
+@app.route('/message/get', methods=['GET'])
+def get_user_messages_by_token():
+    headers = request.headers
+
+    if "Authorization" not in headers:
+        return "{}", 400 #Bad Request
+
+    token = headers.get("Authorization")
+
+    user = database_helper.read_user_by_token(token)
+    if user is None:
+        return "{}", 403 #Forbidden, user not connected
+
+    result = database_helper.read_message(user.email)
+
+    return jsonify_messages(result), 200 #OK
+
+
+@app.route('/message/get/<email>', methods=['GET'])
+def get_user_messages_by_email(email):
+    headers = request.headers
+
+    if "Authorization" not in headers:
+        return "{}", 400 #Bad Request
+
+    token = headers.get("Authorization")
+    user = database_helper.read_user_by_token(token)
+
+    if user is None:
+        return "{}", 403 #Forbidden, user not connected
+
+    if database_helper.read_user(email) is None:
+        return "{}", 404 #Not Found
+
+    result = database_helper.read_message(email)
+
+    return jsonify_messages(result), 200 #OK
+
+def jsonify_messages(messages_result):
+    message_info = []
+    for message in messages_result:
+        message_info.append({
+            "owner": message.owner,
+            "message": message.message,
+            "author": message.author
+        });
+
+    return jsonify(message_info)
+
+
+@app.route('/message/post', methods=['POST'])
+def post_message():
+    json = request.get_json()
+    if "owner" not in json or "message" not in json or "author" not in json:
+        return "{}", 400 #Bad Request
+
+    result = database_helper.create_message(json)
+
+    if result is DatabaseErrorCode.IntegrityError or len(json["message"]) > 1000:
+        return "{}", 400   #Bad request, owner/author not valid or over caracter limits
+
+    if result is not DatabaseErrorCode.Success:
+        return "{}", 500  #Internal server error
+
+    return "{}", 201    #Created
+
+if __name__ == '__main__':
+    app.run()
---
+++ lab03/static/client.js
@@ -0,0 +1,291 @@
+window.onload = function(){
+  loadPage();
+}
+
+// GENERAL FUCTIONS
+
+function loadPage() {
+  let pageContent = document.getElementById("pageContent");
+  let token = localStorage.getItem("token");
+
+  if (token != null) {
+    let content = document.getElementById("profileView").innerHTML;
+    pageContent.innerHTML = content;
+    homeTabClicked();
+  } else {
+    let content = document.getElementById("welcomeView").innerHTML;
+    pageContent.innerHTML = content;
+  }
+}
+
+function showErrors(errorMessages){
+  errorMessageBlock = document.getElementById("errorMessage")
+  errorMessageBlock.innerHTML = "";
+
+  errorMessages = [...new Set(errorMessages)]; //eliminates duplicates
+  errorMessages.forEach(function(message){
+    errorMessageBlock.innerHTML += '<p>' + message + "</p>";
+  });
+
+  setTimeout(function(){
+    errorMessageBlock.innerHTML = "";
+  }, 5000);
+}
+
+// WELCOME VIEW FUNCTONS
+
+function validateFormAndShowErrors(form) {
+  let password = form.newPassword;
+  let repeat = form.repeatPSW;
+
+  if (password.value != repeat.value) {
+    showErrors(["Repeating password does not match!"]);
+    return false;
+  }
+  return true;
+};
+
+function submitSignUpForm(form) {
+  if (!validateFormAndShowErrors(form)) {
+    return;
+  }
+
+  signUpDto = {
+    "email": form.emailSignup.value,
+    "password": form.newPassword.value,
+    "firstname": form.firstName.value,
+    "familyname": form.familyName.value,
+    "gender": form.gender.value,
+    "city": form.city.value,
+    "country": form.country.value
+  };
+
+  let result = serverstub.signUp(signUpDto);
+
+  if (result.success) {
+    let username = signUpDto.email;
+    let password = signUpDto.password;
+
+    signIn(username, password);
+  }
+  else {
+    showErrors([result.message]);
+  }
+};
+
+function submitSignInForm(form) {
+  let username = form.emailLogin.value;
+  let password = form.password.value;
+
+  signIn(username, password);
+};
+
+function signIn(username, password){
+  let result = serverstub.signIn(username, password);
+
+  if (result.success) {
+    localStorage.setItem("token", result.data);
+
+    loadPage();
+  }else {
+    showErrors([result.message]);
+  }
+}
+
+// MAIN PAGE FUNCITONS
+
+function homeTabClicked() {
+  tabClicked("home");
+
+  let token = localStorage.getItem("token");
+  let userDataResult = serverstub.getUserDataByToken(token);
+  let userMessagesResult = serverstub.getUserMessagesByToken(token);
+
+  let errorMessages = [];
+  if (!userDataResult.success){
+    errorMessages.push(userDataResult.message);
+  }
+  if (!userMessagesResult.success){
+    errorMessages.push(userDataResult.message);
+  }
+
+  if (errorMessages.length > 0) {
+    showErrors(errorMessages);
+  }else {
+    localStorage.setItem("viewedUserEmail", userDataResult.data.email);
+
+    loadUserViewToHomePanel(userDataResult.data, userMessagesResult.data, "homePanel");
+  }
+}
+
+function tabClicked(name){
+  setVisiblePanel(name + "Panel");
+  setSelectedTab(name + "Tab");
+}
+
+function setVisiblePanel(name) {
+  document.getElementById("homePanel").style.display = "none";
+  document.getElementById("browsePanel").style.display = "none";
+  document.getElementById("accountPanel").style.display = "none";
+
+  document.getElementById(name).style.display = "block";
+}
+
+function setSelectedTab(name) {
+  document.getElementById("homeTab").style.color = "blue";
+  document.getElementById("browseTab").style.color = "blue";
+  document.getElementById("accountTab").style.color = "blue";
+
+  document.getElementById(name).style.color = "maroon";
+}
+
+function loadUserViewToHomePanel(userData, userMessages) {
+  document.getElementById("homePanel").innerHTML = document.getElementById("userHomeView").innerHTML;
+
+  document.getElementById("firstNameLabel").innerHTML = "Name: " + userData.firstname;
+  document.getElementById("familyNameLabel").innerHTML = "Family Name: " + userData.familyname;
+  document.getElementById("emailLabel").innerHTML = "Email: " + userData.email;
+  document.getElementById("genderLabel").innerHTML = "Gender: " + userData.gender;
+  document.getElementById("cityLabel").innerHTML = "City: " + userData.city;
+  document.getElementById("countryLabel").innerHTML = "Country: " + userData.country;
+
+  showUserMessageWall(userMessages, "userWallHome");
+}
+
+
+function showUserMessageWall(userMessages, panel) {
+  let userWall = document.getElementById(panel);
+  userWall.innerHTML = '<button type="button" class="refreshWallButton" onclick="refreshWallButtonClicked();">Refresh</button>' +
+  '<div class="vertical"><h2>Messages wall</h2></div>';
+
+  let messagesHTML = "";
+  userMessages.forEach(function(message, idx){
+    let author = message.writer;
+    let content = message.content.replaceAll("\n", "<br>");
+
+    messagesHTML += "<h4>Author: " + author + "</h4>";
+    messagesHTML += "<p>" + content + "</p>";
+    if (idx != userMessages.length - 1) {
+      messagesHTML += "<hr>";
+    }
+  });
+
+  userWall.innerHTML += messagesHTML;
+}
+
+function postButtonClicked() {
+  let inHomePanel = document.getElementById("homePanel").style.display == "block";
+  let viewedUser = inHomePanel ? "viewedUserEmail" : "viewedSearchedUserEmail";
+  let newPostTextArea = inHomePanel ? "newPostTextAreaHome" : "newPostTextAreaBrowse";
+  let token = localStorage.getItem("token");
+  let email = localStorage.getItem(viewedUser);
+  let message = document.getElementById(newPostTextArea).value;
+
+  if (message.length == 0){
+    showErrors(["Empty posts not allowed!"]);
+    return;
+  }
+
+  let result = serverstub.postMessage(token, message, email);
+
+  if (!result.success){
+    showErrors([result.message]);
+  }else{
+    document.getElementById(newPostTextArea).value = "";
+    refreshWallButtonClicked();
+  }
+}
+
+// Browse Panel functions
+function searchUser(form){
+  let email = form.emailSearched.value;
+  let token = localStorage.getItem("token");
+  let userDataResult = serverstub.getUserDataByEmail(token, email);
+  let userMessagesResult = serverstub.getUserMessagesByEmail(token, email);
+
+  let errorMessages = [];
+  if (!userDataResult.success){
+    errorMessages.push(userDataResult.message);
+  }
+  if (!userMessagesResult.success){
+    errorMessages.push(userDataResult.message);
+  }
+
+  if (errorMessages.length > 0) {
+    showErrors(errorMessages);
+  }else {
+    localStorage.setItem("viewedSearchedUserEmail", userDataResult.data.email);
+    form.emailSearched.value = "";
+
+    loadUserViewToBrowsePanel(userDataResult.data, userMessagesResult.data);
+  }
+}
+
+function loadUserViewToBrowsePanel(userData, userMessages) {
+  document.getElementById("userPanel").innerHTML = document.getElementById("userHomeViewBrowse").innerHTML;
+
+  document.getElementById("firstNameLabelBrowse").innerHTML = "Name: " + userData.firstname;
+  document.getElementById("familyNameLabelBrowse").innerHTML = "Family Name: " + userData.familyname;
+  document.getElementById("emailLabelBrowse").innerHTML = "Email: " + userData.email;
+  document.getElementById("genderLabelBrowse").innerHTML = "Gender: " + userData.gender;
+  document.getElementById("cityLabelBrowse").innerHTML = "City: " + userData.city;
+  document.getElementById("countryLabelBrowse").innerHTML = "Country: " + userData.country;
+
+  showUserMessageWall(userMessages, "userWallBrowse");
+}
+
+function refreshWallButtonClicked() {
+  let token = localStorage.getItem("token");
+  let inHomePanel = document.getElementById("homePanel").style.display == "block";
+  let panel = inHomePanel ? "userWallHome" : "userWallBrowse";
+  let userMessagesResult = serverstub.getUserMessagesByToken(token);
+  if (!inHomePanel){
+    let email = localStorage.getItem("viewedSearchedUserEmail");
+    userMessagesResult = serverstub.getUserMessagesByEmail(token, email);
+  }
+
+  if (!userMessagesResult.success){
+    showErrors([userMessagesResult.message]);
+  }else{
+    showUserMessageWall(userMessagesResult.data, panel);
+  }
+}
+
+function submitChangePasswordForm(form) {
+  document.getElementById("messagePasswordChange").innerHTML = "";
+  let oldPassword = form.oldPassword.value;
+  let newPassword = form.newPassword.value;
+  let repeatPSW = form.repeatPSW.value;
+
+  let token = localStorage.getItem("token");
+
+  if (!validateFormAndShowErrors(form)) {
+    return;
+  }
+  let passwordChangeResult = serverstub.changePassword(token, oldPassword, newPassword);
+  if (!passwordChangeResult.success){
+    showErrors([passwordChangeResult.message]);
+  } else if (!validateFormAndShowErrors(form)) {
+    return;
+  } else {
+    form.oldPassword.value = "";
+    form.newPassword.value = "";
+    form.repeatPSW.value = "";
+    document.getElementById("messagePasswordChange").innerHTML = passwordChangeResult.message;
+
+    setTimeout(function(){
+      document.getElementById("messagePasswordChange").innerHTML = "";
+    }, 5000);
+  };
+}
+
+function submitSignOut() {
+  let token = localStorage.getItem("token");
+  let signOutResult = serverstub.signOut(token);
+  if (!signOutResult.success){
+    showErrors([signOutResult.message]);
+  } else {
+    localStorage.removeItem("token");
+    loadPage();
+  };
+}
