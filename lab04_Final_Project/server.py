f2900e4545efec60c27e4fbfee69750a4dac4c8b
Vasyl Haievyi <vasha375@student.liu.se>
2022-03-14 13:36:14 +0100

    Lab 04. Google login https redirect fix.

--- lab04_Final_Project/server.py
+++ lab04_Final_Project/server.py
@@ -15,7 +15,10 @@ import utils
 import hmac
 import hashlib
 
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 socketio = SocketIO(app)
 bcrypt = Bcrypt(app)
 
