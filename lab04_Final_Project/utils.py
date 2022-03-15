ffe7f3055a8fd3fcda0610fc5fc39489fedcc051
Sabrina Cazzolato <sabca270@student.liu.se>
2022-03-15 17:17:41 +0100

    Documentation utils.py

--- lab04_Final_Project/utils.py
+++ lab04_Final_Project/utils.py
@@ -2,5 +2,12 @@ import string
 import random
 
 def generate_token():
+    """Generates and returns a token from a randomized sequence of uppercase characters and digits.
+
+    Returns
+    -------
+    str
+        Token
+    """
     random_characters = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 36))
     return str(random_characters)
